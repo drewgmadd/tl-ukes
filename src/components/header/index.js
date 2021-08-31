46ccabd702e4fa5afc3fa907b9568f63851ca605
drewgmadd <andrew12madden@gmail.com>
2021-08-31 15:00:01 -0600

    first commit

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="navbar">
+            <h1 className="title">True Love Ukelele's</h1>
+        </div>
+    )
+}
+
+export default Header;
